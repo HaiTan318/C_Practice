cc39aa6309add097ebf90eeb60dc8ad397e5a160
HaiTan318 <haitan.nguyenduc318@gmail.com>
2026-07-10 23:23:16 +0700

    Add Tpms

--- Day05-Poiters/Bai4_tpms.c
+++ Day05-Poiters/Bai4_tpms.c
@@ -12,16 +12,32 @@ void in_trang_thai(float *arr, char **ten, int n){
         }
     }
 }
-
+void tim_banh_thap_nhat(float *arr, char **ten, int n,
+                             float *ap_suat_min, int *vi_tri)
+{
+    int i = 0;
+    *ap_suat_min = arr[i];
+    *vi_tri = 0;
+    for(i = 0; i < n; i++){
+        if(*(arr + i) < *ap_suat_min){
+            *ap_suat_min = *(arr + i);
+            *vi_tri = i;
+        }
+    }
+}
 
 
 int main(void){
+    float ap_suat_min = 0.0f;
+    int vi_tri = 0;
+    
     float ap_suat[4] = {32.5f, 28.0f, 35.2f, 22.3f}; /*PSI*/
     char *ten_banh[4] = {"Truoc trai", "Truoc phai",
                              "Sau trai", "Sau phai"};
     in_trang_thai(ap_suat, ten_banh, 4);
 
-
+        tim_banh_thap_nhat(ap_suat, ten_banh, 4, &ap_suat_min, &vi_tri);
+        printf("Ap suat thap nhat: %s - %.1f PSI\n", ten_banh[vi_tri], ap_suat_min);
 
     return 0;
 }
