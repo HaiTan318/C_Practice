5e555b6b5ba3fc734dfba5f193b70dff07bc7857
HaiTan318 <haitan.nguyenduc318@gmail.com>
2026-07-17 15:20:23 +0700

    Add Dashboard

---
+++ Day06-Struct/Bai02-Ecu_Dashboard.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+typedef struct{
+    char ten[20];
+    float gia_tri;    /*Gia tri do duoc*/
+    float nguong_max; /*Nguong canh bao*/
+    int don_vi;       /*0=C, 1=V, 2=RPM , 3=atm*/ 
+} CamBien_t;
+
+void kiem_tra_canh_bao(CamBien_t *s){
+    int i = 0;
+    for(i = 0; i < 4; i++){
+        if(s[i].gia_tri > s[i].nguong_max){
+            printf("Canh bao vuot nguong\n");
+        }else{
+            printf("He thong binh thuong\n");
+        }
+    }
+}
+void in_dashboard(CamBien_t *arr, int n){
+    int j = 0;
+
+    char * ten_don_vi[4] = {"C", "V", "RPM", "atm"};
+    printf("ECU DASHBOARD\n");
+
+    for ( j = 0; j < n; j++){
+        if(arr[j].gia_tri > arr[j].nguong_max){
+            printf("%d | %s | %.2f %s | Canh bao vuot nguong\n", j + 1, arr[j].ten, arr[j].gia_tri, ten_don_vi[arr[j].don_vi]);
+        }else{
+            printf("%d | %s | %.2f %s | He thong binh thuong\n", j + 1, arr[j].ten, arr[j].gia_tri, ten_don_vi[arr[j].don_vi]);
+        }
+    }
+}
+int main(void){
+    CamBien_t sensor[4];
+
+    strcpy(sensor[0].ten, "Nhiet do");
+    sensor[0].gia_tri = 60.0f;
+    sensor[0].nguong_max = 80.0f;
+    sensor[0].don_vi = 0;
+
+    strcpy(sensor[1].ten, "Dien ap");
+    sensor[1].gia_tri = 220.0f;
+    sensor[1].nguong_max = 400.0f;
+    sensor[1].don_vi = 1;
+
+    strcpy(sensor[2].ten, "RPM");
+    sensor[2].gia_tri = 3200.0f;
+    sensor[2].nguong_max = 7000.0f;
+    sensor[2].don_vi = 2;
+
+    strcpy(sensor[3].ten, "Ap suat");
+    sensor[3].gia_tri = 40.0f;
+    sensor[3].nguong_max = 150.0f;
+    sensor[3].don_vi = 3;
+
+    kiem_tra_canh_bao(sensor);
+    in_dashboard(sensor, 4);
+
+    return 0;
+}
